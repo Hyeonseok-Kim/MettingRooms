6088ee3c217d31c6a6266092b03c42a888b5ecee
hyeonseok-K98 <hyeonseok.K98@gmail.com>
2017-08-31 17:58:33 +0900

    m:8, d:31 commit

--- MettingRooms/DataCenter.swift
+++ MettingRooms/DataCenter.swift
@@ -10,12 +10,45 @@ import Foundation
 
 
 let dataCenter:DataCenter = DataCenter()
+let fileName = "BranchData.brch"
 
 class DataCenter {
     var branches:[Branch] = []
     
+    var filePath:String { get {
+            let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            return documentDirectory + fileName
+        }}
+    
+    /*init() {
+        
+        if FileManager.default.fileExists(atPath: self.filePath) {
+            let unarchArray = NSKeyedUnarchiver.unarchiveObject(withFile: self.filePath)
+        } else {
+            branches = branches + defaultData()
+        }
+    }*/
     
     init() {
+        
+        if FileManager.default.fileExists(atPath: self.filePath){
+            //read
+            if let unarchArray = NSKeyedUnarchiver.unarchiveObject(withFile: self.filePath) as? [Branch] {
+                branches = branches + unarchArray
+            }
+        }else{
+            //create
+            branches = branches + defaultData()
+        }
+        
+    }
+    
+    func save() {
+        NSKeyedArchiver.archiveRootObject(self.branches, toFile: self.filePath)
+    }
+    
+    
+    func defaultData() -> Array<Branch> {
         let JejuRoom = MeetingRoom(name:"Jeju/제주", capacity: 4)
         let BermudaRoom = MeetingRoom(name:"Bermuda/버뮤다", capacity: 6)
         let SicilyRoom = MeetingRoom(name:"Sicily/시실리", capacity: 6)
@@ -38,30 +71,49 @@ class DataCenter {
         
         pangyoBranch.services = [meetingRoomService, visitorService, vehicleService, deskService]
         
-        branches += [pangyoBranch, samsungBranch, yeoksamBranch, sinrimBranch, songdoBranch, anamBranch]
+        return [pangyoBranch, samsungBranch, yeoksamBranch, sinrimBranch, songdoBranch, anamBranch]
     }
-    
 }
 
-class Branch {
+class Branch:NSObject, NSCoding {
     let name:String
     var services:[Service]?
     
     init(name:String) {
         self.name = name
     }
+    
+    required init?(coder aDecoder: NSCoder){
+        self.name = aDecoder.decodeObject(forKey: "name") as! String
+        self.services = aDecoder.decodeObject(forKey: "services") as? [Service]
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(self.name, forKey: "name")
+        aCoder.encode(self.services, forKey: "services")
+    }
 }
 
-class Service {
+class Service:NSObject, NSCoding {
     let name:String
     var items:[MeetingRoom]?
     
     init(name:String) {
         self.name = name
     }
+    
+    required init?(coder aDecoder: NSCoder){
+        self.name = aDecoder.decodeObject(forKey: "name") as! String
+        self.items = aDecoder.decodeObject(forKey: "items") as? [MeetingRoom]
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(self.name, forKey: "name")
+        aCoder.encode(self.items, forKey: "items")
+    }
 }
 
-class MeetingRoom {
+class MeetingRoom:NSObject, NSCoding {
     let name:String
     var capacity:Int
     var reservations:[Reservation]?
@@ -69,21 +121,49 @@ class MeetingRoom {
         self.name = name
         self.capacity = capacity
     }
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.name = aDecoder.decodeObject(forKey: "name") as! String
+        self.capacity = aDecoder.decodeInteger(forKey: "capacity")
+        self.reservations = aDecoder.decodeObject(forKey: "reservations") as? [Reservation]
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(self.name, forKey: "name")
+        aCoder.encode(self.capacity, forKey: "capacity")
+        aCoder.encode(self.reservations, forKey: "reservations")
+    }
 }
 
 
-class Reservation {
+class Reservation:NSObject, NSCoding {
     var hostName:String
     var date:NSDate
     var attendees:Int
     var equipments:[String]
     var catering:Bool
     
-    init(){
+    override init(){
         self.hostName = "Host of Meeting"
         self.date = NSDate()
         self.attendees = 1
         self.equipments = []
         self.catering = false
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.hostName = aDecoder.decodeObject(forKey: "hostName") as! String
+        self.date = aDecoder.decodeObject(forKey: "date") as! NSDate
+        self.attendees = aDecoder.decodeInteger(forKey: "attendees")
+        self.equipments = aDecoder.decodeObject(forKey: "equipments") as! [String]
+        self.catering = aDecoder.decodeBool(forKey: "catering")
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(self.hostName, forKey: "hostName")
+        aCoder.encode(self.date, forKey: "date")
+        aCoder.encode(self.attendees, forKey: "attendees")
+        aCoder.encode(self.equipments, forKey: "equipments")
+        aCoder.encode(self.catering, forKey: "catering")
     }
 }
