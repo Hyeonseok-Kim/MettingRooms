7fcdec88b1190c614c383d25e12e49e7fdaf519a
hyeonseok-K98 <hyeonseok.K98@gmail.com>
2017-09-25 14:42:50 +0900

    m:9, d:25 Cell delete function Backup before start

--- ReservationListViewController.swift
+++ ReservationListViewController.swift
@@ -78,7 +78,7 @@ class ReservationListViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -88,7 +88,7 @@ class ReservationListViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
