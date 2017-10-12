b70601664339d511de1d6082e18df24a5168f80f
hyeonseok-K98 <hyeonseok.K98@gmail.com>
2017-10-12 20:33:33 +0900

    locationLabel color changed

--- RoomInfoViewController.swift
+++ RoomInfoViewController.swift
@@ -32,6 +32,14 @@ class RoomInfoViewController: UITableViewController {
         
     }
 
+    func UIColorFromHex(rgbValue:UInt32, alpha:Double=1.0)->UIColor {
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
+        let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
+        let blue = CGFloat(rgbValue & 0xFF)/256.0
+        
+        return UIColor(red:red, green:green, blue:blue, alpha:CGFloat(alpha))
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
@@ -73,9 +81,12 @@ class RoomInfoViewController: UITableViewController {
 //            return cell
             return infomationTableCell
         }
-
+        
         let locationRoom = infomation["Location"] as? String ?? ""
         locationCell.textLabel?.text = locationRoom
+        locationCell.backgroundColor = UIColorFromHex(rgbValue: 0x555562, alpha: 1)
+        locationCell.textLabel?.textColor = UIColorFromHex(rgbValue: 0xEBEBF1, alpha: 1)
+        locationCell.textLabel?.font = UIFont.systemFont(ofSize: 15)
         
 //        capacityCell.textLabel?.text = infomation["Capacity"] as? String
 //        basicFacilitiesCell.textLabel?.text = infomation["BasicFacilities"] as? String
